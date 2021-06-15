e4d36be4a2a52e62db5535f7f2c597d0b21e7714
unknown <elekberzadeh333@gmail.com>
2021-06-15 07:08:50 +0400

    image sizes fixed,news page adaptive made

--- js/header.js
+++ js/header.js
@@ -2,11 +2,11 @@ const header = document.querySelector('.header');
 const toggleLangList = () => {
     header.querySelector('.lang-list').classList.toggle('lang-list--show');
 }
-document.querySelector('.lang-select__header').addEventListener('click', toggleLangList);
-document.querySelector('.lang-list').querySelectorAll('li').forEach((e) => {
-    e.addEventListener('click', toggleLangList);
-})
-header.querySelector('.lang-list').addEventListener('mouseleave', toggleLangList);
+// document.querySelector('.lang-select__header').addEventListener('click', toggleLangList);
+// document.querySelector('.lang-list').querySelectorAll('li').forEach((e) => {
+//     e.addEventListener('click', toggleLangList);
+// })
+
 header.querySelector('.header__mobile-toggle').addEventListener('click', (e) => {
     //  document.querySelector('.header-upper').classList.toggle('header-upper--full');
     header.querySelector('.mobile-menu').classList.toggle('mobile-menu--active');
@@ -19,7 +19,6 @@ header.querySelectorAll('.mobile-menu .drop-header').forEach((e, i) => e.addEven
         if (header.querySelector('.drop--active') === header.querySelectorAll('.mobile-menu .drop')[i]) {
             wasSelectedActive = true;
         }
-
         const prevActive = header.querySelector('.drop--active');
         !wasSelectedActive && prevActive.classList.remove('drop--active');
     }
